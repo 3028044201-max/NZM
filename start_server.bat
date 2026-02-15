59b49b9c180e7632fc6dfe7685f62123a8a8f666
3028044201-max <3028044201@qq.com>
2026-02-15 16:20:35 +0800

    Update start_server.bat

--- start_server.bat
+++ start_server.bat
@@ -1,5 +1,5 @@
 Starting NZM Stats Web Server...
-Please wait for the address (e.g. http://localhost:8787) to appear.
+Please wait for the address (e.g. https://localhost:8787) to appear.
 ⛅️ wrangler dev v3.x.x
 Your worker has access to the following bindings:
-⛅️ Listening on http://0.0.0.0:8787
+⛅️ Listening on https://0.0.0.0:8787
