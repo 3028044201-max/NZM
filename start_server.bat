618fa2ad1b946082db08e7bf5b5ffba18d59de31
3028044201-max <3028044201@qq.com>
2026-02-15 16:11:29 +0800

    Remove batch commands and start server directly

--- start_server.bat
+++ start_server.bat
@@ -1,6 +1,5 @@
-@echo off
-cd /d "%~dp0"
-echo Starting NZM Stats Web Server...
-echo Please wait for the address (e.g. http://localhost:8787) to appear.
-npm start
-pause
+Starting NZM Stats Web Server...
+Please wait for the address (e.g. http://localhost:8787) to appear.
+⛅️ wrangler dev v3.x.x
+Your worker has access to the following bindings:
+⛅️ Listening on http://0.0.0.0:8787
